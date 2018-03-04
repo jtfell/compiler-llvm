a4cf816f6d5447014b249b6c04569f057227d1d6
Julian Fell <jtfell@users.noreply.github.com>
2018-03-04 15:55:50 +1000

    working block-based allocation

---
+++ lib/gc/main.c
@@ -0,0 +1,14 @@
+#include "./lib.h"
+
+int main(int argc, char *argv[]) {
+  gc_pool_init();
+
+  bdescr * blockA = alloc_group(5);
+  print_state();
+  bdescr * blockB = alloc_group(11);
+//   print_state();
+//   free_group(blockA);
+//   print_state();
+//   free_group(blockB);
+//   print_state();
+}
